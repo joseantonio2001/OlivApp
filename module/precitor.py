c4d3da28268375cfd5c23854aee590043cf57a89
Carlos-SE <carlossalaseiroa@gmail.com>
2022-10-28 20:55:31 +0200

    Avanza #13, se crea la clase para la lógica de negocio

---
+++ module/precitor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from dataclasses import dataclass
+from .cosecha_anual import CosechaAnual
+
+
+"""Clase generadora de predicciones en base a los 5 años anteriores"""
+@dataclass
+class Predictor():
+
+    year: int
+    cosechas_anteriores: list[CosechaAnual] 
+    cosecha_pred: CosechaAnual = None
+
+    def gen_prediccion(self):
+        # Debe generar un archivo, nombrado como el año que se va a predecir
+        # guardar los datos en el formato establecido en los issues
+        continue
+
+    def gen_prediccion(self, output_file: str):
+        # Debe generar un archivo, nombrado como el argumento output_file
+        # guardar los datos en el formato establecido en los issues
+        continue
+
+    def cargar_prediccion(self):
+        input_file = f'prediccion_{self.year}.txt'
+        self.cosecha_pred = CosechaAnual(input_file)
+
+    def cargar_prediccion(self, input_file: str):
+        self.cosecha_pred = CosechaAnual(input_file)

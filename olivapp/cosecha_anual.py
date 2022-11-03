b3bfefe90e134482aa8f63af82b19e9dc8aaa5b3
Carlos-SE <carlossalaseiroa@gmail.com>
2022-11-03 13:53:56 +0100

    Closes #17, se termina con la estructura que almacena la cosecha anual

--- olivapp/cosecha_anual.py
+++ olivapp/cosecha_anual.py
@@ -6,13 +6,16 @@ from .meses import Mes
 @dataclass(frozen=True)
 class CosechaAnual:
 
+    # Año que representa
     year: int
 
+    # Colecciones de datos mensuales
     evolucion_precios: dict
     produccion: dict
     precipitaciones: dict
     existencias_iniciales: dict
 
+    # Datos anuales
     precio_max: float
     mes_precio_max: Mes
     meses_evaluables: Mes

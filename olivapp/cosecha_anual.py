6da319cdc0d85babedff8dcbc0f070cae637bb06
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-26 00:44:34 +0100

    Constructo para ordenar datos + getters. Close #29

--- olivapp/cosecha_anual.py
+++ olivapp/cosecha_anual.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 from dataclasses import dataclass
-from .meses import Mes
+from meses import Mes
 
-@dataclass(frozen=True)
+#@dataclass(frozen=True)
 class CosechaAnual:
 
     # Año que representa
@@ -11,11 +11,77 @@ class CosechaAnual:
 
     # Colecciones de datos mensuales
     evolucion_precios: dict
+    existencias_iniciales: dict
     produccion: dict
     precipitaciones: dict
-    existencias_iniciales: dict
     # Datos anuales
     precio_max: float
     mes_precio_max: Mes
     meses_evaluables: Mes
+
+   
+    # Constructor personalizado para generar el objeto en base a datos en fichero
+    def __init__(self, input_file: str):
+
+        self.__evolucion_precios = dict()
+        self.__existencias_iniciales = dict()
+        self.__produccion = dict()
+        self.__precipitaciones = dict()
+        self.__precio_maximo = float()
+
+
+        with open(input_file, 'r') as f:
+            
+            lineas = f.readlines()
+            
+            self.__year = int(lineas[0].split(',')[0])
+
+
+        for linea in lineas [3:]:
+            
+            valores = linea.split(',')
+            mes = (Mes[valores[0].upper()]).name
+            precio_mes = valores[1]
+            existencias_iniciales = valores[2]
+            produccion = valores[3]
+            precipitacion  = valores[4]
+
+            self.__evolucion_precios[mes] = precio_mes
+            self.__existencias_iniciales[mes] = existencias_iniciales
+            self.__produccion[mes] = produccion
+   
+
+        for m in Mes:
+
+            if self.__evolucion_precios[m.name] != 's/c':
+                self.__meses_evaluables = m.name
+
+                if(self.__precio_maximo < float(self.__evolucion_precios[m.name])):
+                   self.__precio_maximo = float(self.__evolucion_precios[m.name])
+                   self.__mes_precio_maximo = m.name
+            
+            else:
+                break
+                
+        
+    
+    def get_evolucion_precios(self) -> dict:
+        return self.__evolucion_precios
+    
+    
+    def get_produccion(self) -> dict:
+        return self.__produccion
+    
+    
+    
+    def get_precipitaciones(self) -> dict:
+        return self.__precipitaciones
+    
+    
+    def get_existencias_iniciales(self) -> dict:
+        return self.__existencias_iniciales
+    
+    
+    def get_precio_maximo(self) -> float:
+        return self.__precio_maximo
  

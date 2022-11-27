ea696991ff4d740426cde4c56ab9b987d2e4be90
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-27 18:05:38 +0100

    Constructor + getters clase Predictoe. Close #31

--- olivapp/preditor.py
+++ olivapp/preditor.py
@@ -3,11 +3,25 @@
 from dataclasses import dataclass
 from .cosecha_anual import CosechaAnual
 
-@dataclass
 class Predictor:
 
     year: int
             
-    cosechas_anteriores: list[CosechaAnual]
-    cosecha_pred: CosechaAnual = None
+    cosechas_anteriores: list
+    cosecha_actual: CosechaAnual = None
 
+    # Constructor personalizado para cargar las cosechas
+    def __init__(self, cosechas_anteriores: list, cosecha_actual: CosechaAnual):
+
+        self.__year = cosecha_actual.get_anio()
+        self.__cosechas_anteriores = cosechas_anteriores
+        self.__cosecha_actual = cosecha_actual
+
+    def get_anio(self) -> int:
+        return self.__year
+
+    def get_cosechas_anteriores(self) -> list:
+        return self.__cosechas_anteriores
+
+    def get_cosecha_actual(self) -> CosechaAnual:
+        return self.__cosecha_actual

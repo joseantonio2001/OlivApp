390184972abb0d86d751e017dd5d456a282ca8bf
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-12-06 11:13:13 +0100

    Ajustando mutabilidad de la clase

--- olivapp/cosecha_anual.py
+++ olivapp/cosecha_anual.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from .meses import Mes
 
-#@dataclass(frozen=True)
+@dataclass(frozen=True)
 class CosechaAnual:
 
     # Año que representa
@@ -19,74 +19,75 @@ class CosechaAnual:
     mes_precio_max: Mes
     meses_evaluables: Mes
 
-   
-    # Constructor personalizado para generar el objeto en base a datos en fichero
-    def __init__(self, input_file: str):
-
-        self.__evolucion_precios = dict()
-        self.__existencias_iniciales = dict()
-        self.__produccion = dict()
-        self.__precipitaciones = dict()
-        self.__precio_maximo = float()
-
-
-        with open('./informes/'+input_file, 'r') as f:
-            
-            lineas = f.readlines()
-            
-            self.__year = int(lineas[0].split(',')[0])
-
-
-        for linea in lineas [3:]:
-            
-            valores = linea.split(',')
-            mes = (Mes[valores[0].upper()]).name
-            precio_mes = valores[1]
-            existencias_iniciales = valores[2]
-            produccion = valores[3]
-            precipitacion  = valores[4]
-
-            self.__evolucion_precios[mes] = precio_mes
-            self.__existencias_iniciales[mes] = existencias_iniciales
-            self.__produccion[mes] = produccion
-            self.__precipitaciones[mes] = precipitacion 
-
-            if self.__evolucion_precios[mes] != 's/c':
-                self.__meses_evaluables = mes
-
-                if(self.__precio_maximo < float(self.__evolucion_precios[mes])):
-                   self.__precio_maximo = float(self.__evolucion_precios[mes])
-                   self.__mes_precio_maximo = mes
-
 
     def get_anio(self) -> int:
-        return self.__year
+        return self.year
 
 
     def get_evolucion_precios(self) -> dict:
-        return self.__evolucion_precios
+        return self.evolucion_precios
     
     
     def get_produccion(self) -> dict:
-        return self.__produccion
+        return self.produccion
     
     
     
     def get_precipitaciones(self) -> dict:
-        return self.__precipitaciones
+        return self.precipitaciones
     
     
     def get_existencias_iniciales(self) -> dict:
-        return self.__existencias_iniciales
+        return self.existencias_iniciales
     
     
     def get_precio_maximo(self) -> float:
-        return self.__precio_maximo
+        return self.precio_maximo
  
 
     def get_mes_prec_máx(self) -> Mes:
-        return self.__mes_precio_maximo
+        return self.mes_precio_max
 
 
     def get_meses_evaluables(self) -> Mes:
-        return self.__meses_evaluables
+        return self.meses_evaluables
+
+
+
+# Inicializador de la clase Cosecha Anual
+
+def CosechaAnual_init(input_file: str):
+
+    year = int()
+    evolucion_precios = dict()
+    existencias_iniciales = dict()
+    produccion = dict()
+    precipitaciones = dict()
+    precio_max = float()
+    mes_precio_max = str()
+    meses_evaluables = str()
+
+    with open('./informes/'+ input_file, 'r') as f:
+        
+        lineas = f.readlines()
+        
+        year = int(lineas[0].split(',')[0])
+
+
+    for linea in lineas [3:]:
+        
+        valores = linea.split(',')
+        mes = (Mes[valores[0].upper()]).name
+        evolucion_precios[mes] = valores[1]
+        existencias_iniciales[mes] = valores[2]
+        produccion[mes] = valores[3]
+        precipitaciones[mes]  = valores[4]
+
+        if evolucion_precios[mes] != 's/c':
+            meses_evaluables = mes
+
+            if(precio_max < float(evolucion_precios[mes])):
+                precio_max = float(evolucion_precios[mes])
+                mes_precio_max = mes
+
+    return CosechaAnual(year, evolucion_precios, existencias_iniciales, produccion, precipitaciones, precio_max, mes_precio_max, meses_evaluables)

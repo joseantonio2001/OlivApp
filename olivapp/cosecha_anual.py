af073ad35d1223422223e086bf6f7e0e307af9a6
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-12-16 18:14:33 +0100

    Remove getters

--- olivapp/cosecha_anual.py
+++ olivapp/cosecha_anual.py
@@ -20,37 +20,7 @@ class CosechaAnual:
     meses_evaluables: Mes
 
 
-    def get_anio(self) -> int:
-        return self.year
 
-
-    def get_evolucion_precios(self) -> dict:
-        return self.evolucion_precios
-    
-    
-    def get_produccion(self) -> dict:
-        return self.produccion
-    
-    
-    
-    def get_precipitaciones(self) -> dict:
-        return self.precipitaciones
-    
-    
-    def get_existencias_iniciales(self) -> dict:
-        return self.existencias_iniciales
-    
-    
-    def get_precio_maximo(self) -> float:
-        return self.precio_maximo
- 
-
-    def get_mes_prec_máx(self) -> Mes:
-        return self.mes_precio_max
-
-
-    def get_meses_evaluables(self) -> Mes:
-        return self.meses_evaluables
 
 
 
